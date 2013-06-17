29595e8c2a0b4ee9a2a39fa2b6e7961983f97aea
Soufii <abbshr@outlook.com>
2013-06-17 17:54:45 +0800

    update: make port 80 availible

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(flash());
