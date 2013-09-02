b8c4daf5e51408d102fa2192f0f25d94f4755df0
Soufii <abbshr@outlook.com>
2013-09-02 22:54:13 +0800

    ~

--- routes/ctrlfunction/getRealTags.js
+++ routes/ctrlfunction/getRealTags.js
@@ -1,15 +1,13 @@
 function getRealTags(tags) {       //获得一个符合规范的标签数组,传入参数数组不能为['']或['\n\t\s']
-	tags = tags.join(",");
-	tags = tags.match(/([^,]+)(?!.*,\1(,|$))/ig);   //去除重复标签
-	console.log(tags);
 	var i = 0;
-	while (tags[i]) {
+	while (tags[i]) {  //去空
 		if (tags[i] == 0 && !(/0/i).test(tags[i]))
 			tags.splice(i, 1);
 		else 
 			i++;
 	}
-	console.log(tags);
+	tags = tags.join(",");
+	tags = tags.match(/([^,]+)(?!.*,\1(,|$))/ig) || [''];   //去除重复标签
 	return tags;
 }
 
